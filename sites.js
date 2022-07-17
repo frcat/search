9b0336c1370fcdfe2fe0da5e5b5c74a224c8862d
French-Cat <src@frcat.win>
2022-07-17 16:43:32 -0400

    + sort sites

--- sites.js
+++ sites.js
@@ -1,44 +1,44 @@
 var main = {
-  npm: "https://npmjs.org/package/{q}",
-  github: "https://github.com/search?q={q}",
-  stackoverflow: "https://stackoverflow.com/search?q={q}",
-  discordjs: "https://discord.js.org/#/docs/main/stable/search?query={q}",
-  lighthouse: "https://developers.google.com/speed/pagespeed/insights/?url={q}",
-  dns: "https://www.nslookup.io/dns-records/{q}",
-  mdn: "https://developer.mozilla.org/en-US/search?q={q}",
-  duckduckgo: "https://duckduckgo.com/?q={q}",
-  spotify: "https://open.spotify.com/search/{q}",
-  invite: "https://discord.gg/{q}",
-  youtube: "https://www.youtube.com/results?search_query={q}",
-  twitch: "https://www.twitch.tv/{q}",
   amazon: "https://www.amazon.com/s?k={q}",
-  repo: "https://repo.new",
+  cloudflare: "https://dash.cloudflare.com",
+  discordjs: "https://discord.js.org/#/docs/main/stable/search?query={q}",
+  dns: "https://www.nslookup.io/dns-records/{q}",
+  duckduckgo: "https://duckduckgo.com/?q={q}",
   gist: "https://gist.new",
-  repl: "https://replit.new/{q}",
+  github: "https://github.com/search?q={q}",
   glitch: "https://glitch.new/{q}",
+  invite: "https://discord.gg/{q}",
+  lighthouse: "https://developers.google.com/speed/pagespeed/insights/?url={q}",
+  mdn: "https://developer.mozilla.org/en-US/search?q={q}",
+  npm: "https://npmjs.org/package/{q}",
+  pages: "https://dash.cloudflare.com/?account=pages",
   presencedb: "https://www.presencedb.com/search?q={q}",
+  repl: "https://replit.new/{q}",
+  repo: "https://repo.new",
+  spotify: "https://open.spotify.com/search/{q}",
+  stackoverflow: "https://stackoverflow.com/search?q={q}",
   translate: "https://translate.google.com/?sl=auto&tl=en&text={q}&op=translate",
   tweet: "https://mobile.twitter.com/compose/tweet?text={q}",
-  pages: "https://dash.cloudflare.com/?account=pages",
+  twitch: "https://www.twitch.tv/{q}",
   workers: "https://dash.cloudflare.com/?account=workers",
-  cloudflare: "https://dash.cloudflare.com"
+  youtube: "https://www.youtube.com/results?search_query={q}",
 };
 
 var alt = {
-  gh: "https://github.com/search?q={q}",
-  git: "https://github.com/search?q={q}",
-  djs: "https://discord.js.org/#/docs/main/stable/search?query={q}",
-  ddg: "https://duckduckgo.com/?q={q}",
-  duck: "https://duckduckgo.com/?q={q}",
-  yt: "https://www.youtube.com/results?search_query={q}",
+  cf: "https://dash.cloudflare.com",
   cfpages: "https://dash.cloudflare.com/?account=pages",
   cfworkers: "https://dash.cloudflare.com/?account=workers",
-  cf: "https://dash.cloudflare.com",
+  ddg: "https://duckduckgo.com/?q={q}",
+  djs: "https://discord.js.org/#/docs/main/stable/search?query={q}",
+  duck: "https://duckduckgo.com/?q={q}",
+  gh: "https://github.com/search?q={q}",
+  git: "https://github.com/search?q={q}",
   pdb: "https://www.presencedb.com/search?q={q}",
   presdb: "https://www.presencedb.com/search?q={q}",
   trans: "https://translate.google.com/?sl=auto&tl=en&text={q}&op=translate",
-}
+  yt: "https://www.youtube.com/results?search_query={q}"
+};
 
-var SITES = Object.assign(main, alt)
+var SITES = Object.assign(main, alt);
 
-export { SITES }
+export { SITES };
