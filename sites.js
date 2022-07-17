c0404c9248013b8417da13936890275557307ab4
French-Cat <src@frcat.win>
2022-07-17 16:39:50 -0400

    + sites

--- sites.js
+++ sites.js
@@ -9,15 +9,19 @@ var main = {
   duckduckgo: "https://duckduckgo.com/?q={q}",
   spotify: "https://open.spotify.com/search/{q}",
   invite: "https://discord.gg/{q}",
-  youtube: "https://www.youtube.com/results?search_query={q}&page={startPage?}&utm_source=opensearch",
+  youtube: "https://www.youtube.com/results?search_query={q}",
   twitch: "https://www.twitch.tv/{q}",
   amazon: "https://www.amazon.com/s?k={q}",
   repo: "https://repo.new",
   gist: "https://gist.new",
   repl: "https://replit.new/{q}",
+  glitch: "https://glitch.new/{q}",
   presencedb: "https://www.presencedb.com/search?q={q}",
   translate: "https://translate.google.com/?sl=auto&tl=en&text={q}&op=translate",
   tweet: "https://mobile.twitter.com/compose/tweet?text={q}",
+  pages: "https://dash.cloudflare.com/?account=pages",
+  workers: "https://dash.cloudflare.com/?account=workers",
+  cloudflare: "https://dash.cloudflare.com"
 };
 
 var alt = {
@@ -27,6 +31,12 @@ var alt = {
   ddg: "https://duckduckgo.com/?q={q}",
   duck: "https://duckduckgo.com/?q={q}",
   yt: "https://www.youtube.com/results?search_query={q}",
+  cfpages: "https://dash.cloudflare.com/?account=pages",
+  cfworkers: "https://dash.cloudflare.com/?account=workers",
+  cf: "https://dash.cloudflare.com",
+  pdb: "https://www.presencedb.com/search?q={q}",
+  presdb: "https://www.presencedb.com/search?q={q}",
+  trans: "https://translate.google.com/?sl=auto&tl=en&text={q}&op=translate",
 }
 
 var SITES = Object.assign(main, alt)
